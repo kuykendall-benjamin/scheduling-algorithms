801c8999978e3e482c6acf9abcf6dfe4ce155552
Benjamin Kuykendall <kuykendall.ben@gmail.com>
2016-12-15 15:03:51 -0500

    conllu parse

--- code.py
+++ code.py
@@ -1,12 +1,64 @@
 import numpy as np
 from cvxpy import *
 
-n = 3;
+# tree
+class Node:
+    def __init__(self, i, children=[]):
+        self.i = i
+        self.children = children
 
-E = [(0,2), (1,2)]
-J = [[0,2], [1]]
+    def __str__(self):
+        if self.children:
+            return "(%d %s)" % (self.i, " ".join(str(c) for c in self.children))
+        else:
+            return "(%d)" % (self.i)
 
-def choose_speeds(n, E, J):
+    def add_child(self, c):
+        self.children.append(c)
+
+    def get_constraints(self, S, R, constraints):
+        for c in self.children:
+            constraints.append(S[c.i] + R[c.i] <= S[self.i])
+            # print("%d < %d" % (c.i, self.i))
+            c.get_constraints(S, R, constraints)
+
+def conllu_tree(fn):
+    sentences = []
+    s = []
+    i = 0
+    with open(fn, 'r') as f:
+        for line in f:
+            i = i + 1
+            if line[0] == '#': # new sentence
+                sentences.append(s)
+                s = []
+            else: # read word line
+                sp = line.split('\t')
+                try:
+                    v = int(sp[0])
+                    u = int(sp[6])
+                    s.append((v, u))
+                except:
+                    print('bad line %d "%s"' % (i, line[:-1]))
+        sentences.append(s)
+    return sentences[1:]
+
+# edge list => Node
+def from_edges(E, n=None):
+    if not n: # assume tree
+        n = len(E) + 1
+    nodes = [Node(i, []) for i in range(0, n)]
+    for i,j in E:
+        nodes[j].add_child(nodes[i])
+    return nodes[0]
+
+#print(from_edges(conllu_tree('smpl.conllu')[0]))
+
+# evaluate convex program to choose speeds given machine assignment over jobs [1...n]
+# n integer number of jobs
+# T a list of root Nodes
+# J machine assignment 2D array
+def choose_speeds(n, T, J):
     S = Variable(n)
     R = Variable(n)
     constraints = []
@@ -21,8 +73,8 @@ def choose_speeds(n, E, J):
         constraints.append(S[i] + R[i] <= 1)
 
     # precedence
-    for i, j in E:
-        constraints.append(S[i] + R[i] <= S[j])
+    for N in T:
+        N.get_constraints(S, R, constraints)
 
     # one job at a time
     for A in J:
@@ -39,4 +91,7 @@ def choose_speeds(n, E, J):
     return (prob.status, prob.value, S.value, R.value)
 
 
-print(choose_speeds(n, E, J))
+#n = 3;
+#T = Node(2, [Node(1), Node(0)])
+#J = [[0,2], [1]]
+#print(choose_speeds(n, [T], J))
