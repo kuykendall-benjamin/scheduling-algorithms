fcf45408e3cf6f457064a10f0e1eedce3bf396e7
Benjamin Kuykendall <kuykendall.ben@gmail.com>
2016-12-15 21:50:35 -0500

    aprox alg

--- code.py
+++ code.py
@@ -86,11 +86,12 @@ def parse_schedule(J, res):
 
 # tree
 class Node:
-    def __init__(self, i, children=[]):
+    def __init__(self, i, children=None):
         self.i = i
-        self.children = children
+        self.children = children if children is not None else []
         self.schedulable = True
         self.parent = None
+        self.chain_height = None
 
     def __str__(self):
         if self.children:
@@ -158,11 +159,40 @@ class Node:
             self.children.remove(n)
         return n
 
+    def assign_chain_height(self):
+        for c in self.children:
+            c.assign_chain_height()
+        if not self.children:
+            self.chain_height = 1
+        elif len(self.children) == 1:
+            self.chain_height = self.children[0].chain_height + 1
+        else:
+            self.chain_height = max(c.chain_height for c in self.children) + 1
+
+    def chains(self, prec=[]):
+        if not self.children:
+            return [(prec + [self], self.chain_height)]
+        if len(self.children) == 1:
+            return self.children[0].chains(prec + [self])
+        else:
+            res = [(prec + [self], self.chain_height)]
+            for c in self.children:
+                res = res + c.chains()
+            return res
+
+    def chain_decompose(self):
+        self.assign_chain_height()
+        res = [[] for i in range(self.chain_height)]
+        for chain, height in self.chains():
+            res[height-1].append([c.i for c in chain])
+        return [r for r in res if r]
+
+
 # edge list => Node
 def from_edges(E, n=None):
     if not n: # assume tree
         n = len(E) + 1
-    nodes = [Node(i, []) for i in range(0, n)]
+    nodes = [Node(i, []) for i in range(n)]
     for i,j in E:
         nodes[j].add_child(nodes[i])
     return nodes[0]
@@ -187,7 +217,7 @@ def conllu_tree(fn):
                     print('bad line %d "%s"' % (i, line[:-1]))
     return [from_edges(s) for s in sentences]
 
-def schedule(m, T, rand=False, zz=False):
+def schedule(m, T, rand=False, zz=False, optimize=True):
     n = T.size()
     E = T.get_edges()
     J = [[] for i in range(m)]
@@ -214,14 +244,96 @@ def schedule(m, T, rand=False, zz=False):
             mach.append(j.value())
             # print(j, end="\t")
             if j == T:
-                # print()
-                return (J, choose_speeds(n,E,J))
-        # print()
+                print()
+                if optimize: # todo: break into two methods
+                    return (J, choose_speeds(n,E,J))
+                else:
+                    return (J, None)
+        print()
+
+def schedule_aa(m, blocks):
+    print(blocks)
+    n = sum(sum(len(l) for l in b) for b in blocks)
+
+    JS = []
+    ID = []
+
+    # assign each block using existing alg
+    for b in range(len(blocks)):
+        i = 1
+        ids = {}
+        J = []
+
+        # to tree
+        root = Node(0)
+        for l in blocks[b]:
+            prev = []
+            for j in reversed(l):
+                prev = [Node(i, prev)]
+                ids[i] = j
+                i = i + 1
+            root.add_child(prev[0])
+
+        # assign to machines
+        J, _ = schedule(m, root, optimize=False)
+
+        # remove root (always last so does not change assignment)
+        for mach in J:
+            if mach and mach[-1] == 0:
+                mach.pop(-1)
+
+        JS.append(J)
+        ID.append(ids)
+
+    # scale each block to constant width
+    total = sum(len(J[0]) for J in JS)
+    off = 0
+    S = [0]*n
+    R = [0]*n
+    J_ALL = [[] for _ in range(m)]
+    for b in range(len(blocks)):
+        J = JS[b]
+        I = ID[b]
+        c = len(J[0]) / total
+        print(c)
+        for i in range(len(J)):
+            if not J[i]:
+                continue
+            inc = c / len(J[i])
+            print("  ", len(J[i]), " * ", inc)
+            for j in range(len(J[i])):
+                S[I[J[i][j]]] = off + inc*j
+                R[I[J[i][j]]] = inc
+                J_ALL[i].append(I[J[i][j]])
+        off += c
 
 
-tree = conllu_tree('smpl.conllu')[0]
-print(tree)
-J, S = schedule(3, tree)
+    return (J_ALL, ("ok", None, S, R))
+
+
+
+#tree = conllu_tree('smpl.conllu')[0]
+#for b in tree.chain_decompose():
+#    print(b)
+# J, S = schedule_aa(3, tree.chain_decompose())
+# print(J)
+# print(S)
+# tikz(J, S, "out.tikz")
+
+#tree = Node(0, [Node(7, [Node(1, [Node(2), Node(3)])]),
+#                Node(4, [Node(5), Node(6)])])
+
+# print(tree)
+# J, S = schedule(3, tree)
+#print(J)
+
+
+#tree = conllu_tree('smpl.conllu')[0]
+#print(tree)
+#J, S = schedule(3, tree)
+
+#print(S)
+#tikz(J, S, "out.tikz")
 
 #J, S = schedule(3, Node(0, [Node(1, [Node(2), Node(3)]),
 #                            Node(4, [Node(5), Node(6)])]), zz=True)
@@ -241,3 +353,7 @@ tikz(parsed, "out.tikz")
 #T = Node(2, [Node(1), Node(0)])
 #J = [[0,2], [1]]
 #print(choose_speeds(n, [T], J))
+
+#blocks = (tree.chain_decompose())
+#for b in blocks:
+#    print(b)
